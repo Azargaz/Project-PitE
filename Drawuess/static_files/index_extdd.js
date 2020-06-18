1c076e1f57c98734002d33dad22569656d7b5707
Gordwick <sitkosebastian97@gmail.com>
2020-06-18 21:19:02 +0200

    final res

--- Drawuess/static_files/index_extdd.js
+++ Drawuess/static_files/index_extdd.js
@@ -17,29 +17,18 @@ const answerButtonYes = () => {
     answerElement = document.getElementById('answer')
     console.log(`a: ${answer}\np: ${similar_to}`);
     if(answer === similar_to) {
-        answerElement.innerHTML= 'GREAT!!'
+        answerElement.innerHTML= 'GREAT!!';
     }
     else{
-        answerElement.innerHTML= 'That wasn\'t what you should have drawn.'
+        answerElement.innerHTML= 'That wasn\'t what you should have drawn.';
     }
 
 }
 
 const answerButtonNo = () => {
     answerElement = document.getElementById('answer')
-    answerElement.innerHTML= 'That\'s a pity, try to draw again <button onclick = clearCanvas() class="btn btn-secondary btn-sm"> Clear </button>'
+    answerElement.innerHTML= 'That\'s a pity, try to draw again <button onclick = clearCanvas() class="btn btn-secondary btn-sm"> Clear </button>';
 
 }
 
-// function check_if_true(guessed_image)
-// {
-//     answerElement = document.getElementById('answer');
-//     if(guessed_image == answer)
-//     {
-//         updateGoodAnswer();
-//     }
-//     else{
-//         updateBadAnswer();
-//     }
-// }
 
